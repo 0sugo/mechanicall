59cec545136db000b1a6b5bd52bcc9e6738a67b9
0sugo <josugo26@gmail.com>
2023-11-23 11:07:52 +0300

    Add class to retrieve mechanics

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Mechanic
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+class MechanicListView(ListView):
+    model = Mechanic
+    template_name = 'mechanic-list.html'
+    context_object_name = 'mechanics'
+    # return render(request,'mechanic-list.html')
