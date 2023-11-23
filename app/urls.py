5ea32d0fa3e01584ca4cfa354551d9ff3522c94c
0sugo <josugo26@gmail.com>
2023-11-23 11:08:22 +0300

    Add url to access view mechanics

--- app/urls.py
+++ app/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from .views import MechanicListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'),
+    path('mechanics/', MechanicListView.as_view(), name='mechanic-list'),
 ]
+ 
