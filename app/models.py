809b459484269648c24e921367027436c1480c7f
0sugo <josugo26@gmail.com>
2023-11-24 19:44:43 +0300

    Add tow trucks data

--- app/models.py
+++ app/models.py
@@ -13,3 +13,29 @@ class Mechanic(models.Model):
 
     def __str__(self):
         return self.firstname+" "+self.lastname
+
+class ServiceRequest(models.Model):
+    id = models.AutoField(primary_key=True)
+    location = models.CharField(max_length=40)
+    faulty_part = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.location+" - "+self.faulty_part
+
+class Tow(models.Model):
+    id = models.AutoField(primary_key=True)
+    companyname = models.CharField(max_length=40)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.companyname+" - "+self.location
+
+class TowRequest(models.Model):
+    id = models.AutoField(primary_key =True)
+    location = models.CharField(max_length=50)
+    destination = models.CharField(max_length=50)
+    def __str__(self):
+        return self.location+" - "+self.destination
