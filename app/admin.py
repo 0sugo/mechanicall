a4be9938ab1d4858a7640a8e3a419e41bd585ff6
0sugo <josugo26@gmail.com>
2023-11-24 19:44:25 +0300

    Roll migrations

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Mechanic
+from .models import Mechanic,ServiceRequest,Tow,TowRequest
 # Register your models here.
 
 admin.site.register(Mechanic)
+admin.site.register(Tow)
+admin.site.register(ServiceRequest)
+admin.site.register(TowRequest)
