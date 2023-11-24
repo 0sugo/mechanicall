dc8cbe187e70354ef0dac717058befabfd095e17
0sugo <josugo26@gmail.com>
2023-11-24 19:45:05 +0300

    Add views methods for trucks

--- app/views.py
+++ app/views.py
@@ -1,13 +1,39 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Mechanic
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from mechanicall.forms import ServiceRequestForm,TowRequestForm
+from .models import  Mechanic
+from .models import Tow
 def index(request):
     return render(request, 'index.html')
 
-class MechanicListView(ListView):
-    model = Mechanic
-    template_name = 'mechanic-list.html'
-    context_object_name = 'mechanics'
-    # return render(request,'mechanic-list.html')
+def mechanic_list(request):
+    mechanics = Mechanic.objects.all()
+
+    if request.method == 'POST':
+        location = request.POST.get('location', '')
+        faulty_part = request.POST.get('faulty_part', '')
+        print(f"Location: {location}, Faulty Part: {faulty_part}")
+
+        mechanics = Mechanic.objects.filter(location=location, specialisation=faulty_part)
+
+        form = ServiceRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            print("Form NOT valid")
+    return render(request, 'mechanic-list.html', {'mechanics': mechanics})
+
+def tow_list(request):
+    tows = Tow.objects.all()
+    form = TowRequestForm(request.POST)
+
+    if request.method=='POST':
+
+        if form.is_valid():
+            form.save()
+            location = request.POST.get('location', '')
+            destination = request.POST.get('destination', '')
+            print(f"Location: {location},destination: {destination}")
+            tows = Tow.objects.filter(location=location)
+        else:
+            print("Form not valid")
+    return render(request, 'tow-list.html', {'tows':tows})
