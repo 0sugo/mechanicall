8c39f513174c0b3255cee50de41bab93b5db52a0
0sugo <josugo26@gmail.com>
2023-11-25 19:17:37 +0300

    Update added tables

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Mechanic,ServiceRequest,Tow,TowRequest
+from .models import Mechanic,ServiceRequest,Tow,TowRequest,User
 # Register your models here.
 
 admin.site.register(Mechanic)
 admin.site.register(Tow)
 admin.site.register(ServiceRequest)
 admin.site.register(TowRequest)
+admin.site.register(User)
