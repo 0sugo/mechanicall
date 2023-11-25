fe33fc287394efe520a1d6d9f037052a790f0cb4
0sugo <josugo26@gmail.com>
2023-11-25 19:18:22 +0300

    Update added templates url

--- app/urls.py
+++ app/urls.py
@@ -20,8 +20,13 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login,name='login'),
+    path('register/', views.register,name='register'),
     path('', views.index,name='index'),
     path('mechanics-list/', views.mechanic_list, name='mechanic_list'),
     path('tow-list/', views.tow_list, name='tow_list'),
+    path('toggle-mechanic-status/<int:mechanic_id>/', views.toggle_mechanic_status, name='toggle_mechanic_status'),
+    path('toggle-tow-status/<int:tow_id>/', views.toggle_tow_status, name='toggle_tow_status'),
+    path('login/', views.login, name='login'),
 ]
  
