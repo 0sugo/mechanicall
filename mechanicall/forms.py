ad873a284bce653f06881bd4340a920031bef6b6
0sugo <josugo26@gmail.com>
2023-11-28 23:34:01 +0300

    Update forms.py

--- mechanicall/forms.py
+++ mechanicall/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from app.models import ServiceRequest,TowRequest
+from app.models import ServiceRequest,TowRequest,Mechanic,Tow
 
 class ServiceRequestForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,13 @@ class TowRequestForm(forms.ModelForm):
     class Meta:
         model = TowRequest
         fields = ['location','destination']
+
+class MechanicForm(forms.ModelForm):
+    class Meta:
+        model = Mechanic
+        fields = ['firstname', 'lastname', 'email', 'phone', 'location', 'specialisation', 'carbrand', 'password']
+
+class TowForm(forms.ModelForm):
+    class Meta:
+        model = Tow
+        fields = ['companyname', 'email', 'password', 'phone', 'location']
